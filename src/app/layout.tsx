1277b0b88ce935b90638217b8fe12422c1d5ae1c
Yuxuan Liu <yuxuanl@spokeo.com>
2026-06-13 16:19:37 -0700

    feat: actual analytics packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Inter({
@@ -156,6 +157,7 @@ export default function RootLayout({
         />
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
