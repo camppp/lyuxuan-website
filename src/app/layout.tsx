a395e5841692f29a60327ee7fda958f2906be3a6
Yuxuan Liu <yuxuanl@spokeo.com>
2026-06-13 16:18:11 -0700

    fix: add component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistSans = Inter({
@@ -154,6 +155,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(personStructuredData) }}
         />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
